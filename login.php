fcb85d64f573cd5ebf80ab592df59223cc647f8f
mjoost <michelle@limitlessvirtual.com>
2015-10-07 11:18:11 +0200

    Login_page

---
+++ login.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8" />
+<title>Login</title>
+ 
+<link rel="stylesheet" href="stylesheets/styles.css" type="text/css" />
+
+</head>
+ 
+<body class="main-wrapper">
+    <div class="row">
+        <div class="col-md-12">
+            <h1 class="txt-center heading-txt">SIGN UP</h1>
+            <div class="row">
+                <div class="col-md-4">
+                </div>
+                <div class="col-md-4">
+                     <div class="row">
+                        <div class="col-md-4">
+                            <h4 class="sub-txt">Email</h4>
+                            <h4 class="sub-txt">Password</h4>
+                        </div>
+                         <div class="col-md-8">
+                             <form>
+                                  <input type="email" class="form-control" id="InputEmail1" placeholder="Type your email address">                
+                                 <input type="password" class="form-control" id="InputPassword1" placeholder="Type your password">
+                        </div>
+                     </div>
+                    <button type="submit" class="btn btn-default submit-btn">Submit</button>
+                    </form>
+                </div>
+                <div class="col-md-4">
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+    
+<script src="javascripts/bootstrap.min.js"></script>    
+</html>
