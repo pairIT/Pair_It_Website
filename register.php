2a43fb3a7b9b8e56830c46361676aaab392c30ca
mjoost <michelle@limitlessvirtual.com>
2015-10-18 09:18:16 +0200

    Register

--- register.php
+++ register.php
@@ -2,30 +2,33 @@
 <html lang="en">
 <head>
 <meta charset="utf-8" />
-<title>Login</title>
+<title>Register</title>
  
 <link rel="stylesheet" href="stylesheets/styles.css" type="text/css" />
 
 </head>
- 
+
 <body class="main-wrapper">
     <div class="row">
         <div class="col-md-12">
-            <h1 class="txt-center heading-txt">SIGN UP</h1>
+            <h1 class="txt-center heading-txt">Register</h1>
             <div class="row">
                 <div class="col-md-4">
                 </div>
                 <div class="col-md-4">
                      <div class="row">
                         <div class="col-md-4">
-                            <h4 class="sub-txt">Email</h4>
-                            <h4 class="sub-txt" style="margin-top:25px;">Password</h4>
+                            <h4 class="sub-txt">Name</h4>
+                            <h4 class="sub-txt" style="margin-top:25px;">Email</h4>
+                            <h4 class="sub-txt" style="margin-top:30px;">Password</h4>
+                            <h4 class="sub-txt" style="margin-top:30px; margin-left:-100px;">Confirm Password</h4>
                         </div>
                          <div class="col-md-8">
                              <form>
-                                  <input type="email" class="form-control" id="InputEmail1" placeholder="Type your email address">                
-                                 <br>
-                                 <input type="password" class="form-control" id="InputPassword1" placeholder="Type your password">
+                                <input type="name" class="form-control" id="InputName" placeholder="Type your name"><br>              
+                                <input type="email" class="form-control" id="InputEmail1" placeholder="Type your email address"><br>
+                                 <input type="password" class="form-control" id="InputPassword1" placeholder="Type your password"><br>
+                                 <input type="password" class="form-control" id="InputPasswordConfirm" placeholder="Confirm your password">
                         </div>
                      </div>
                     <br>
