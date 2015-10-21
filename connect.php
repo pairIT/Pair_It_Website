3d6f1fd58de5a597e57ecbeadb7a52ed4f2320b1
beeha <s.banubhai@gmail.com>
2015-10-21 15:34:54 +0200

    connect_localhost

--- connect.php
+++ connect.php
@@ -1,9 +1,9 @@
 <?php
     $username="root";
     $password="root";
-    $database="TestPairit";
+    $database="testpairit";
 
-    mysql_connect(localhost,$username,$password);
+    mysql_connect('localhost:8889', 'root', 'root');
     @mysql_select_db($database) or die("Database Error");
 ?>
 
