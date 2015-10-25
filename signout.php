f05544c7c8586a3693422be59fe1b86a7e872b4c
mjoost <michelle@limitlessvirtual.com>
2015-10-25 11:26:04 +0200

    SignOut

--- signout.php
+++ signout.php
@@ -1,9 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8" />
+<title>Sign Out</title>
+ 
+<link rel="stylesheet" href="stylesheets/styles.css" type="text/css" />
+
+</head>
+
 <?php
         session_start();
         session_destroy();
 ?>
-
+    
 <html>
-<body>
-    <h1>You have been signed out, <a href="signin.php">sign in again</a></h1>
-    </body></html>
+<body class="main-wrapper">
+    <div class="row">
+        <div class="col-md-12">
+            <h1 class="txt-center heading-txt">SIGN OUT</h1>
+            <div class="row">
+                <div class="col-sm-4"></div>
+                <div class="col-sm-4">
+                    <h4 class="sub-txt" style="text-align:center;">You have been signed out</h4>
+                    <br>
+                    <a href="login.php"><button type="button" value="Refresh" class="btn btn-default submit-btn">Sign in again</button></a>
+                </div>
+                <div class="col-sm-4"></div>
+            </div>
+        </div>
+    </div>
+</body>
+    
+</html>
